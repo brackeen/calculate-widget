b38ce5dcc18e917c573af9fca699b9ffc7b5ef6c
David Brackeen <brackeen@gmail.com>
2020-03-11 10:31:55 -0700

    Add input field

--- Calculate/ViewController.swift
+++ Calculate/ViewController.swift
@@ -9,15 +9,47 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var inputField: NSTextField!
+    
+    fileprivate var textFieldWasEmpty = true
+    fileprivate var allowInsertAnsVariable = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        inputField.stringValue = "1+1"
+        inputField.selectText(nil)
+        inputField.cell?.focusRingType = .none
+        inputField.becomeFirstResponder()
     }
+    
+    @IBAction func enterPressed(_ sender: Any) {
+        let expression = inputField.stringValue
+        if let result = Calculate.shared.calc(expression, addToHistory: true) {
+            print(result)
+        }
+        
+        inputField.stringValue = ""
+        textFieldWasEmpty = true
+        allowInsertAnsVariable = true
+    }
+}
 
-    override var representedObject: Any? {
-        didSet {
-            // Update the view, if already loaded.
+extension ViewController: NSTextFieldDelegate {
+
+    func controlTextDidChange(_ obj: Notification) {
+        let text = inputField.stringValue
+        if text.isEmpty {
+            textFieldWasEmpty = true
+        } else {
+            if let ch = text.first, textFieldWasEmpty && allowInsertAnsVariable &&
+                (ch == "+" || ch == "-" || ch == "*" || ch == "/" || ch == "%" || ch == "&" || ch == "|" || ch == "^") {
+                inputField.stringValue = "ans" + text
+                inputField.currentEditor()?.moveToEndOfLine(nil)
+                allowInsertAnsVariable = false
+            }
+            textFieldWasEmpty = false
         }
     }
 }

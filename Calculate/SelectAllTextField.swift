17b14fed24095aa0405f3e5e4faed50b3b859986
David Brackeen <brackeen@gmail.com>
2020-03-21 14:53:25 -0700

    Select labels on click

---
+++ Calculate/SelectAllTextField.swift
@@ -0,0 +1,19 @@
+//
+//  SelectAllTextField.swift
+//  Calculate
+//
+//  Created by David Brackeen on 3/21/20.
+//  Copyright © 2020 David Brackeen. All rights reserved.
+//
+
+import Cocoa
+
+class SelectAllTextField: NSTextField {
+    
+    override func mouseDown(with event: NSEvent) {
+        super.mouseDown(with: event)
+        if let textEditor = currentEditor() {
+            textEditor.selectAll(self)
+        }
+    }
+}

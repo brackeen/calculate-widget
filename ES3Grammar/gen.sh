b99e2931ebbf173b583900ae5c1387433a80c5bf
David Brackeen <brackeen@gmail.com>
2020-03-26 07:30:33 -0700

    Reorganize files

--- ES3Grammar/gen.sh
+++ ES3Grammar/gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-OUT_DIR=deps/gen
-java -classpath deps/antlr-3.1.2.jar org.antlr.Tool ECMAScript3Ext.g -fo $OUT_DIR
+OUT_DIR=3rdparty/gen
+java -classpath 3rdparty/antlr-3.1.2.jar org.antlr.Tool ECMAScript3Ext.g -fo $OUT_DIR
 rm $OUT_DIR/ECMAScript3Ext.tokens
     
 # ANTLR 3.1.2 inserts invalid code that should be changed.

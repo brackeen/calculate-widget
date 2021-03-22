72599b2d932df99f1fc52a6e90b9e2140f2b4908
David Brackeen <brackeen@gmail.com>
2021-03-22 08:00:57 -0700

    Add equals symbol to break list

--- Calculate/String+Break.swift
+++ Calculate/String+Break.swift
@@ -14,7 +14,7 @@ extension String {
     // Insert zero-width space ("\u200B") after break chars.
     // For example, "3.141592653589793+1.0000000000000000" on a small-width window should break on the "+"
     public func breakOnSymbols() -> String {
-        let symbols = CharacterSet(charactersIn: "+-*/%&|^,;!<>")
+        let symbols = CharacterSet(charactersIn: "+-*/%&|^,;!<>=")
         var prevCharIsWhitespaceOrSymbol = true
         return unicodeScalars.reversed().map ( { ch in
             let isWhitespace = CharacterSet.whitespacesAndNewlines.contains(ch)
--- CalculateTests/CalculateTests.swift
+++ CalculateTests/CalculateTests.swift
@@ -171,7 +171,10 @@ class CalculateTests: XCTestCase {
     
     func testStringBreak() {
         XCTAssertEqual("1 + 2".breakOnSymbols(), "1 + 2")
+        XCTAssertEqual("a=0".breakOnSymbols(), "a=\u{200B}0")
         XCTAssertEqual("a++".breakOnSymbols(), "a++")
+        XCTAssertEqual("a+=1".breakOnSymbols(), "a+=\u{200B}1")
+        XCTAssertEqual("a==2".breakOnSymbols(), "a==\u{200B}2")
         XCTAssertEqual("a++;b++".breakOnSymbols(), "a++;\u{200B}b++")
         XCTAssertEqual("1+2+3".breakOnSymbols(), "1+\u{200B}2+\u{200B}3")
         XCTAssertEqual("1+\u{200B}2+\u{200B}3".removedInvisibleBreaks(), "1+2+3")
